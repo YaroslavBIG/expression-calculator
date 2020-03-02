2673185b0faaa8f4fc50afb7f389ccad86d8f9fd
FireGUN <yaroslavbig@ya.ru>
2020-03-02 22:51:40 +0200

    feet: all work except Brackets 1 test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    const exprArr = expr.split('');
+    //console.log(expr);
+    const openingSymbols = ['(', '[', '{', '<'];
+    const closingSymbols = [')', ']', '}', '>'];
+    const stack = [];
+    let result;
+    for (let bracket of expr) { 
+        //let countBrackets = 0;
+        if (openingSymbols.includes(bracket)) {
+           // countBrackets += 1;
+            stack.push(bracket);
+         //console.log('push', bracket);
+        }
+    };
+
+    for (let bracket of expr) {
+        if (closingSymbols.includes(bracket)) {
+            stack.pop()
+            console.log('push', bracket);
+            //console.log('pop', bracket);
+        } 
+    };
+    console.log(stack, stack.length);
+    result = stack.length === 0 ? true : false;
+    if (!result) {
+        throw new 'ExpressionError: Brackets must be paired';
+    };
     const divisionZero = '/ 0';
     if (expr.includes(divisionZero)) {
         throw new 'TypeError: Division by zero.'
